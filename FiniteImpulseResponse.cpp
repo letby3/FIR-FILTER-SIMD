78ddeccdf06d833eabc00139907be7d817c3d249
letby3 <fvahit2@gmail.com>
2022-11-25 11:23:03 +0300

    V0.15

--- FiniteImpulseResponse.cpp
+++ FiniteImpulseResponse.cpp
@@ -1,7 +1,40 @@
-#include <iostream>
+/*
+g++ -c FiniteImpulseResponse.cpp -I"C:/SFML-2.5.1-mingw/include"
+g++ FiniteImpulseResponse.o -o sfml-app -L"C:/SFML-2.5.1-mingw/lib" -lsfml-graphics -lsfml-window -lsfml-system
+*/
+
+#include <SFML/Graphics.hpp>
+#include <bits/stdc++.h>
 
 using namespace std;
+using namespace sf;
 
-int main(){
-    
+int main()
+{
+        Image img;
+        img.loadFromFile("Test1.jpg");
+        Texture texImg;
+        texImg.loadFromImage(img);
+        Sprite sprImg;
+        sprImg.setTexture(texImg);
+        sprImg.setPosition(0,0);
+
+        RenderWindow window(sf::VideoMode(675, 450), "SFML works!");
+        
+
+        while (window.isOpen())
+        {
+            sf::Event event;
+            while (window.pollEvent(event))
+            {
+                if (event.type == sf::Event::Closed)
+                    window.close();
+            }
+
+            window.clear();
+            window.draw(sprImg);
+            window.display();
+        }
+
+    return 0;
 }
